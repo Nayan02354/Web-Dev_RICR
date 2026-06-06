29d32ba2cd6ee706ff4fe734675ea4377b33f0d5
Nayan <vishwakarmanayan418@gmail.com>
2026-06-06 20:31:03 +0530

    Refactor Contact, Login, and Register components to enhance form handling and layout

--- Frontend/My_First_React_App/src/Pages/Contact_Us.jsx
+++ Frontend/My_First_React_App/src/Pages/Contact_Us.jsx
@@ -1,10 +1,72 @@
+import { useState } from "react";
+
 function Contact_Us() {
+  const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log("userName :", userName);
+    console.log("email :", email);
+    console.log("phone :", phone);
+  };
   return (
     <>
-      <div className="container d-flex justify-content-center align-items-center">
-        <span className="text-primary fs-2">Contact Us</span>
+      <div className="d-flex justify-content-center">
+        <div className="border p-3 w-50 mt-5 bg-light shadow rounded">
+          <h1 className="text-center">Welcome Back!!!</h1>
+          <br />
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="fullName">Full Name</label>
+            <input
+              type="text"
+              name="fullName"
+              className="form-control"
+              value={userName}
+              onChange={(e) => setUserName(e.target.value)}
+            />
+            <br />
+            <label htmlFor="password">Password</label>
+            <input
+              type="text"
+              name="password"
+              className="form-control"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <br />
+            <label htmlFor="email">Enter Email</label>
+            <input
+              type="text"
+              name="email"
+              className="form-control"
+              value={userName}
+              onChange={(e) => setUserName(e.target.value)}
+            />
+            <br />
+            <label htmlFor="phone">Enter Phone No.</label>
+            <input
+              type="number"
+              name="email"
+              className="form-control"
+              value={userName}
+              onChange={(e) => setUserName(e.target.value)}
+            />
+            <br />
+            <input type="radio" />
+            <br />
+            <div className="float-end">
+              <button type="submit" className="btn btn-primary">
+                Login
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </>
   );
 }
+
 export default Contact_Us;
--- Frontend/My_First_React_App/src/Pages/Register.jsx
+++ Frontend/My_First_React_App/src/Pages/Register.jsx
@@ -1,8 +1,75 @@
+import { useState } from "react";
+
 function Register() {
+  const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConirmPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log("userName :", userName);
+    console.log("email :", email);
+    console.log("password :", password);
+    console.log("confirmPassword :", confirmPassword);
+  };
   return (
     <>
-      <div>Register</div>
+      <div className="d-flex justify-content-center">
+        <div className="border p-3 w-50 mt-5 bg-light shadow rounded">
+          <h1 className="text-center">Welcome Back!!!</h1>
+          <br />
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="fullName">Full Name</label>
+            <input
+              type="text"
+              name="fullName"
+              className="form-control"
+              value={userName}
+              onChange={(e) => setUserName(e.target.value)}
+            />
+
+            <br />
+
+            <label htmlFor="email">Enter Email</label>
+            <input
+              type="text"
+              name="email"
+              className="form-control"
+              value={userName}
+              onChange={(e) => setUserName(e.target.value)}
+            />
+            <br />
+            <label htmlFor="password">Password</label>
+            <input
+              type="text"
+              name="password"
+              className="form-control"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <br />
+            <label htmlFor="confirmPassword">Confirm Password</label>
+            <input
+              type="text"
+              name="confirmPassword"
+              className="form-control"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <textarea name="textArea" id="textArea" className="w-75"></textarea>
+            <div className="text-end">
+              <button type="submit" className="btn btn-primary">  
+                Login
+              </button>
+            </div>
+          </form>
+          <hr />
+        </div>
+      </div>
     </>
   );
 }
+
 export default Register;
